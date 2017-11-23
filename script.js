bb3eef11e5f16fe122be3a5ea404a6f78e8b9b0c
lgard13 <lgard13@yahoo.com>
2017-11-22 21:42:26 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -18,7 +18,10 @@ var error = [
 "Now you know and knowing is... wait",
 "There are more holes in this cOde than the holes in Stephen Hawking's blackhole theory",
 "Code Tester #1: She chose this</br>Code Tester #2: She chose that?<br>Shannon: Wait, was that wrong?<br>Code Tester #3: Too late now!<br>All testers laugh!!",
-"The Tao teaches us that there is no right and wrong, only the way. That is until it saw this code."
+"The Tao teaches us that there is no right and wrong, only the way. That is until it saw this code.",
+"An error occured while laughing at your entry",
+"Alarm 1: Don't go on.</br>Alarm 2: Go back while you still can.</br>Alarm 3: This is not the way.</br>Alarm 4: Take heed, and go no further.</br>Alarm 5: Beware, beware.</br>Alarm 6: Soon it will be too late.</br>Shannon: Oh, shut up!</br>Alarm 7: Sorry, just doing my job.</br>Shannon: Well you don't have to do it to me!</br>Alarm 8: Beware, for the...</br>Shannon: Just forget it!</br>Alarm 8: Oh please, I haven't said it for such a long time!</br>Hoggle: Oh, all right, but don't expect a big reaction!</br>Alarm 8: No no no, of course not! (clears throat) \"For the path you will take will lead to certain destruction.\" Thank you very much...",
+"Keyboard not found. Press F1"
 ]
 
 function testCode(){
