592a47dc905a7811d4011bdeccfe98ed1e28b48e
lgard13 <13823949+lgard13@users.noreply.github.com>
2017-11-24 10:57:49 -0600

    Update script.js

--- script.js
+++ script.js
@@ -12,21 +12,20 @@ var clues = [
 {'question':'digging','answer':'You caught that huh. Nice job, but it wasn\'t a clue. At least not as of yet ;)'},
 {'question':'the way','answer':''},
 {'question':'ca·ca·o','answer':'1234'},
-{'question':'TAKEYOURTHRONE','answer':'Your are a QUEEN and QUEENS don\'t compete with hoes.')
- 
+{'question':'TAKEYOURTHRONE','answer':'Your are a QUEEN and QUEENS don\'t compete with peasants.'}
 ]
 
 var error = [
 "Erm... somethings isn't quite right",
 "404: Code not found",
 "Now you know and knowing is... wait",
-"There are more holes in this cOde than the holes in Stephen Hawking's blackhole theory",
+"There are more holes in this code than the holes in Stephen Hawking's blackhole theory",
 "Code Tester #1: She chose this</br>Code Tester #2: She chose that?<br>Shannon: Wait, was that wrong?<br>Code Tester #3: Too late now!<br>All testers laugh!!",
 "The Tao teaches us that there is no right and wrong, only the way. That is until it saw this code.",
 "An error occured while laughing at your entry",
 "Alarm 1: Don't go on.</br>Alarm 2: Go back while you still can.</br>Alarm 3: This is not the way.</br>Alarm 4: Take heed, and go no further.</br>Alarm 5: Beware, beware.</br>Alarm 6: Soon it will be too late.</br>Shannon: Oh, shut up!</br>Alarm 7: Sorry, just doing my job.</br>Shannon: Well you don't have to do it to me!</br>Alarm 8: Beware, for the...</br>Shannon: Just forget it!</br>Alarm 8: Oh please, I haven't said it for such a long time!</br>Hoggle: Oh, all right, but don't expect a big reaction!</br>Alarm 8: No no no, of course not! (clears throat) \"For the path you will take will lead to certain destruction.\" Thank you very much...",
 "Keyboard not found. Press F1",
-"Sometimes even a answer can reaveal <em>the way</em> to the right path"
+"Sometimes even an answer can reaveal <em>the way</em> to the right path"
 ]
 
 function testCode(){
